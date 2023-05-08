753dbcb5920cae911edfc3d6f4b036497a7db781
yuataka <yataka74603@gmail.com>
2023-05-08 22:23:19 +0900

    fix docker run script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,1 +1,10 @@
-docker run -it --rm --user ros --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" ros-noetic-rootless
+PWD=`pwd`
+
+docker run -it --rm \
+	--user rosnoetic \
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    	--volume=${PWD}/src:/home/catkin_ws/src \
+	ros-noetic-rootless \
+	bash
